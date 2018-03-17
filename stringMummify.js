8c459c408854f86b3456b2e90fbae461ab74388c
Gemeng <qingemeng@gmail.com>
2018-03-17 23:21:10 +0800

    Create practice branch without implementation

--- stringMummify.js
+++ stringMummify.js
@@ -1,40 +1,8 @@
 const VOWELS = 'aeiou';
 const REPLACEMENT = 'mummy';
 
-const isAVowel = (char) => {
-  return VOWELS.indexOf(char.toLowerCase()) >= 0;
-};
-
-const isVowelCountLessThan30Percent = (word) => {
-  let vowelCount = 0;
-  const length = word.length;
-  for (let i = 0; i < length; i++) {
-    const letter = word.charAt(i);
-    if (isAVowel(letter)) {
-      vowelCount += 1;
-    }
-  }
-  return length === 0 || vowelCount / length < 0.3
-};
-
-const replaceVowels = (word) => {
-  let mummified = '';
-  for (let i = 0; i < word.length; i++) {
-    const letter = word.charAt(i);
-    if (!isAVowel(letter)) {
-      mummified += letter;
-    } else if (!mummified.endsWith(REPLACEMENT)) {
-      mummified += REPLACEMENT;
-    }
-  }
-  return mummified;
-};
-
 const mummify = (word) => {
-  if(isVowelCountLessThan30Percent(word)){
-    return word;
-  }
-  return replaceVowels(word);
+  
 };
 
 export default mummify;
